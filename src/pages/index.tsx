5515229caa77490d31a96ede1d99f21a7df5a166
Davi SAS <ivad8080@gmail.com>
2021-03-05 12:29:08 -0300

    Minor Updates

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ type Props = {
   title: string
 }
 
-export default function Home({title = 'React Avancado!'}: Props) {
+export default function Home({title = 'React Avancado!!!!!!!!!!'}: Props) {
   return (
     <div className={styles.container}>
       <Head>
